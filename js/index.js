9f743f0c493e41e233d339cbe819a060f3f51b53
aaronmesupe <aaronmp1990@gmail.com>
2017-09-19 09:58:35 +0200

    Nuevo proyecto. Funciona con Firebase

---
+++ js/index.js
@@ -0,0 +1,75 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC_hCkuaECQSEdkmiFrO1Vklctb8EXfQsQ",
+  authDomain: "trivial-film-eoi.firebaseapp.com",
+  databaseURL: "https://trivial-film-eoi.firebaseio.com",
+  projectId: "trivial-film-eoi",
+  storageBucket: "trivial-film-eoi.appspot.com",
+  messagingSenderId: "655951519588"
+};
+firebase.initializeApp(config);
+
+
+$.get('https://trivial-film-eoi.firebaseio.com/.json?print=pretty', function(data) {
+  usuarios = data.usuarios
+  anadirUsuarios(usuarios)
+});
+
+
+limpiar();
+
+function anadirUsuarios(users){
+  nombresUsuarios = Object.keys(users) ;
+
+  $('.jugar').on("click", function(e){
+
+      var coincide = 0;
+
+      usuario = $('.jugador').val();
+      if ( usuario === "" ){
+        alert('Introduce un nombre')
+      }
+
+      else{
+
+        long = nombresUsuarios.length
+
+        for (var i = 0; i < long; i++){
+
+          if (usuario === nombresUsuarios[i]){
+              alert('Ese nombre se está usando. Introduce otro.')
+              coincide = 1;
+              break;
+          }
+
+        }//Fin FOR
+
+        if (coincide == 1){
+          limpiar();
+        }
+        else{
+            setTimeout(move,1500)
+        }//Fin ELSE
+
+
+          
+    }//Fin ELSE
+
+  })//FIN fincion CLICK
+
+}//FIN funcion ANADIRUSUARIOS
+
+
+function limpiar(){
+  $('.jugador').val('');
+}
+
+function numRandom (min, max) {
+  return Math.trunc(Math.random() * (max - min) + min);
+}//FIN funcion NUMRANDOM
+
+
+function move(){
+  url = './preguntas.html?nombre='+usuario;
+  window.location = url;
+}
---
+++ js/tuPregunta.js
@@ -0,0 +1,59 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC_hCkuaECQSEdkmiFrO1Vklctb8EXfQsQ",
+    authDomain: "trivial-film-eoi.firebaseapp.com",
+    databaseURL: "https://trivial-film-eoi.firebaseio.com",
+    projectId: "trivial-film-eoi",
+    storageBucket: "trivial-film-eoi.appspot.com",
+    messagingSenderId: "655951519588"
+};
+firebase.initializeApp(config);
+
+$.get('https://trivial-film-eoi.firebaseio.com/preguntas/.json?print=pretty', function(data) {
+	preguntasAnadidas = data
+	anadirPreguntas(preguntasAnadidas)
+});
+
+
+function anadirPreguntas(preguntasAnadidas){
+
+	long = preguntasAnadidas.length
+
+	$('.enviar-pregunta').on("click", function(e){
+		usuario = $('.usuario').val();
+		titulo = $('.titulo').val();
+		rc = $('.respuestaCorrecta').val();
+		ri1 = $('.ri1').val();
+		ri2 = $('.ri2').val();
+		ri3 = $('.ri3').val();
+		ri4 = $('.ri4').val();
+
+
+		if ( usuario === "" || titulo === "" || rc === "" || ri1 === "" || ri2 === "" || ri3 === "" || ri4 === "" ){
+			var reiniciar = confirm('Faltan campos por reyenar. \n¿Reiniciar formulario?');
+			if (reiniciar == true){
+				limpiar();
+			}
+		}
+		else{
+			dbPregunta = firebase.database().ref().child('preguntas/'+long)
+			dbPregunta.set({"index":long,"nombreUsuario":usuario,"titulo":titulo,
+		 	"respuestaCorrecta":rc,"respuestasIncorrectas":[ri1, ri2, ri3, ri4]});
+		 	limpiar();
+		 	alert('Formulario enviado correctamente');
+
+			
+		}
+
+	})
+}
+
+function limpiar(){
+	$('.usuario').val('');
+	$('.titulo').val('');
+	$('.respuestaCorrecta').val('');
+	$('.ri1').val('');
+	$('.ri2').val('');
+	$('.ri3').val('');
+	$('.ri4').val('');
+}
